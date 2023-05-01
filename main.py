b533baae6f36574f540df63d992285562ced3b8c
thricvz <thricvz@gmail.com>
2023-05-01 20:30:15 +0200

    predict function done

--- main.py
+++ main.py
@@ -4,20 +4,25 @@ import random
 pg.init()                                      #initiates a pygame instance
 board = Board((512,512))
 #board.generate_pieces()
-board.board_state["g1"] = Piece("knight","white")
-board.board_state["e7"] = Piece("queen","white")
+board.board_state["e7"] = Piece("pawn","white")
+board.board_state["e3"] = Piece("bishop","white")
+board.board_state["h3"] = Piece("rook","black")
+board.board_state["d7"] = Piece("knight","white")
+board.board_state["g1"] = Piece("queen","white")
+board.board_state["h2"] = Piece("king","white")
+
 running = True                                  #variable checks if loop should go on
 while running:                                  #game loop
     for event in pg.event.get():
         if event.type == pg.QUIT:               #Once exited the instance gets killed
-            running = False 
-            
-    #gamelogic   
+            running = False
+
+    #gamelogic
     board.build()
-    if board.clicked():
-        board.predict()
+    board.manage_click()
 
     pg.display.flip()
 pg.quit()
+
 
 
--- modules.py
+++ modules.py
@@ -25,12 +25,14 @@ class Board():
                             'a1': None, 'b1': None, 'c1': None, 'd1': None, 'e1': None, 'f1': None, 'g1': None, 'h1': None}
         
         self.board_pos = list(self.board_state)     #list that serves as indicator for the pieces
-        self.already_selected = False
+        self.board_pos = [self.board_pos[x:x+8] for x in range(0, len(self.board_pos), 8)]
+        self.piece_already_selected = False               #responsible for seeeing if a piece is selected
         self.dotted_cases = []
 
     def coords(self,case):
-        case_index = self.board_pos.index(case)  
-        return case_index % 8,case_index // 8
+        x = int(ord(case[0])-97)
+        y = abs(int(case[1]) - 8)
+        return x,y
 
     def generate_pieces(self):
         piece_positions = {
@@ -58,23 +60,24 @@ class Board():
 
                 self.SCREEN.blit(current_tile,(row_number * self.TILE_SIZE,column_number * self.TILE_SIZE))    #draws the tile
 
-        ###to redo 
-        for case in self.board_pos:
-            board_ocupation = self.board_state[case]                #gets what is in the case
-            case_x,case_y = self.coords(case)                                 #ex : case a1 -> 56 in list -> row: 7 column : 0 (count started from 0)
-            
-            text = self.FONT.render(case, True, (255,255,255))             #case text
+        for row in self.board_pos:
+            for case in row:
+                board_ocupation = self.board_state[case]                #gets what is in the case
+                case_x,case_y = self.coords(case)                                 #ex : case a1 -> 56 in list -> row: 7 column : 0 (count started from 0)
 
-            if board_ocupation != None: 
-                self.SCREEN.blit(board_ocupation.sprite,(case_x * self.TILE_SIZE,case_y * self.TILE_SIZE))   #draws the content if it is existent ###need t add adjustement system
-                if board_ocupation.clicked and self.dotted_cases != None:
-                    for dot in self.dotted_cases:
-                        dot_x,dot_y = self.coords(dot)
-                        self.SCREEN.blit(self.DOT,((dot_x - 1) * self.TILE_SIZE + self.TILE_SIZE ,(dot_y - 1) * self.TILE_SIZE + self.TILE_SIZE))
+                text = self.FONT.render(case, True, (255,255,255))             #case text
 
-            self.SCREEN.blit(text,(case_x * self.TILE_SIZE ,case_y * self.TILE_SIZE))
+                if board_ocupation != None:
+                    if board_ocupation.clicked and self.dotted_cases != None:
+                        for dot in self.dotted_cases:
+                            dot_x,dot_y = self.coords(dot)
+                            self.SCREEN.blit(self.DOT,((dot_x - 1) * self.TILE_SIZE + self.TILE_SIZE ,(dot_y - 1) * self.TILE_SIZE + self.TILE_SIZE))
 
-    def clicked(self):
+                    self.SCREEN.blit(board_ocupation.sprite, (case_x * self.TILE_SIZE,case_y * self.TILE_SIZE))  # displays the piece itself
+
+                self.SCREEN.blit(text,(case_x * self.TILE_SIZE ,case_y * self.TILE_SIZE))
+
+    def manage_click(self):
         if pg.mouse.get_pressed()[0]:   #if mouse is pressed then:
                 mouse_x,mouse_y = pg.mouse.get_pos()    #gets the mouse coordinates
                 case_column = (mouse_x // self.TILE_SIZE)   #transforms it into the corresponding row and column (count starts at 1)
@@ -82,59 +85,77 @@ class Board():
 
                 pg.time.delay(200) #delay that prevents the dot from blinking fast
 
-                clicked_case = self.board_pos[case_row * 8 + case_column]
-                self.clicked_piece = self.board_state[clicked_case]
-
-                if not self.already_selected:
-                    self.selected_case = clicked_case   #updating piece status and board attribute
-                    self.clicked_piece.clicked = True
-                    self.already_selected = True
-                    self.dotted_cases = self.predict()
+                current_clicked_case = self.board_pos[case_row][case_column]
 
 
-                elif clicked_case == self.selected_case and self.already_selected:
-                    self.clicked_piece.clicked = False
-                    self.already_selected = False
-                    self.clicked_piece = None
-                    self.dotted_cases = []
-                return True
+                if self.piece_already_selected:
+                    """if current_clicked_case == self.selected_board_case:
+                        self.piece_already_selected = False
 
-        return False
-    def move(self):
-        pass
+                    else :
+                        if current_clicked_case in self.dotted_cases:
+                            self.move(current_clicked_case)
+                    self.piece_already_selected = False"""
+                    pass
+
+                else:
+                    if self.board_state[current_clicked_case] != None:  # if the selected case is a piece
+                        self.selected_board_case = current_clicked_case
+                        self.piece_already_selected = True
+                        self.dotted_cases = self.predict()
+        return self
+    def move(self,new_case):
+        if new_case in self.dotted_cases:
+            self.board_state[new_case] = None
+            self.board_state[new_case] = self.board_state[self.selected_board_case]
+            self.board_state[self.selected_board_case] = None
+            return True     #means that piece has been moved
+        else:
+            return False
+
     def predict(self):  #too much if else statements need to find a more algoritmic way to solve this problem
-        axes = self.clicked_piece.mouvement_axis
-        x = self.board_pos.index(self.selected_case)
-        mouv_type = self.clicked_piece.mouvement_type
-        predicted_positions = []
+        predicted_positions = list()
+        if self.piece_already_selected:
+            piece = self.board_state[self.selected_board_case]      ###p stands for piece (not to polutte overall vision)
+            p_axes = piece.mouvement_axis
+            p_location = self.coords(self.selected_board_case)
+            p_mouvement_type = piece.mouvement_type
 
-        for axis in axes:
-            if mouv_type == "continous":
-                N = 1
-                generated_index = x + N * axis
+            for step in p_axes:
+                for n in range(1, 8): #since the maximum of cases in any direction is 8
 
-                while generated_index in range(len(self.board_pos)):
-                    generated_position =  self.board_pos[generated_index]
-                    #conditions of movement
-                    if self.board_state[generated_position] == None:
-                        predicted_positions.append(generated_index)
-                    elif self.board_state[generated_position].color == self.clicked_piece.color:
+                    if p_mouvement_type == "discontinous": n = 1
+
+                    if p_location[0] + n * step[0] in range(8) and p_location[1] + n * step[1] in range(8):             #checks if the future position  is on the board
+                        gen_x = p_location[0] + n * step[0]
+                        gen_y = p_location[1] + n * step[1]
+
+                        generated_position = self.board_pos[gen_y][gen_x]
+
+                        if self.board_state[generated_position] == None:
+                            predicted_positions.append(generated_position)
+
+                        elif self.board_state[generated_position].color != piece.color:
+                            predicted_positions.append(generated_position)
+                            break
+
+                        elif self.board_state[generated_position].color == piece.color:
+                            break
+                    else:
                         break
-                    elif self.board_state[generated_position] != self.clicked_piece.color:
-                        predicted_positions.append(generated_index)
-                        break
-                    elif N == 8:
-                        break
-                    N += 1
-            else:
-                pos = self.board_pos[x + axis]
-                predicted_positions.append(pos)
-        #now for uncontinous pieces
+
         return predicted_positions
-        pass 
+
 
 class Piece():
-    mouvement_axis_map = {"pawn": [8] ,"rook" : [1,8],"knight" : [6,-6,10,-10,-15,15,17,-17],"bishop" : [7,9] ,"queen" : [1,7,8,9],"king" : [1,7,8,9]}
+    mouvement_axis_map = {
+                    "pawn": [(0,-1)] ,"rook" : [(1,0),(0,-1),(-1,0),(0,1)],
+                    "knight" : [(-2,1),(-1,2),(-2,-1),(-1,-2),(2,1),(1,2),(2,-1),(1,-2)],
+                    "bishop" : [(-1,1),(-1,-1),(1,1),(1,-1)] ,
+                    "queen" : [(1,0),(0,-1),(-1,0),(0,1),(-1,1),(-1,-1),(1,1),(1,-1)],
+                    "king" : [(1,0),(0,-1),(-1,0),(0,1),(-1,1),(-1,-1),(1,1),(1,-1)]
+                          }
+    ###this format consists of the "steps" that have to be done on the board (x,y) : ex-> (-1,1) would be one "step" to the left and one "step" to the bottom
     def __init__(self,type,color):
         self.color = color
         self.type = type
@@ -143,49 +164,3 @@ class Piece():
         self.mouvement_type = "discontinous" if self.type  in ["king","knight","pawn"] else "continous"
         self.times_moved = 0
         self.clicked = False
-
-
-    """def predict(self):
-        current_pos_index  = Board.board_pos.index(self.current_pos)         #gets the numerical value of current position
-        self.possible_moves = []
-
-        ###Iturn code cleaner & the 2 case mouvement from start
-
-        if Board.board_state[Board.board_pos[current_pos_index -  self.mouvement_axis]] == None :             #if the case before him is empty
-            self.possible_moves.append(Board.board_pos[current_pos_index -  self.mouvement_axis])
-        
-        if Board.board_pos[current_pos_index] in Board.board_right_border:           #if pawn on right border
-            if Board.board_state[Board.board_pos[current_pos_index - 9]] != None and Board.board_state[self.board_pos[current_pos_index - 9]].color != self.color:        #if left diagonal not empty
-                self.possible_moves.append(self.board_pos[current_pos_index - 9])                                                                                                             
-
-        elif Board.board_pos[current_pos_index] in Board.board_left_border:
-             if Board.board_state[Board.board_pos[current_pos_index - 7]] != None and Board.board_state[Board.board_pos[current_pos_index - 7]].color != self.color:        #if right diagonal not empty
-                self.possible_moves.append(Board.board_pos[current_pos_index - 7])                   
-
-        else:
-            if Board.board_state[Board.board_pos[current_pos_index - 9]] != None and Board.board_state[Board.board_pos[current_pos_index - 9]].color != self.color:        #if left diagonal not empty
-                self.possible_moves.append(Board.board_pos[current_pos_index - 9])
-
-            elif Board.board_state[self.board_pos[current_pos_index - 7]] != None and Board.board_state[Board.board_pos[current_pos_index - 7]].color != self.color:        #if right diagonal not empty
-                self.possible_moves.append(Board.board_pos[current_pos_index - 7])   
-
-        ###displaying the dots
-        self.possible_moves_indexes = [Board.board_pos.index(coord) for coord in self.possible_moves]
-        for index in self.possible_moves_indexes:
-            row = index // 8                                   
-            column = index % 8  
-            Board.SCREEN.blit(self.dot,(column * 128 + self.dot_size / 2,row * 128 + self.dot_size / 2))
-
-    ####second to complete
-    def place(self):
-        Board.board_state[self.board_pos[self.board_pos.index(self.current_pos)]] = self
-
-    def move(self,pos):      ### need to implement the idea of acessing the pos with mouse click
-        #self.predict()
-        #if pos in self.possible_moves:
-        Board.board_state[pos] = self #puts the element in its future position by using slicing 
-        Board.board_state[Board.board_pos[Board.board_pos.index(self.current_pos)]] = None                        #replaces the precedent position with a void
-        self.current_pos = pos"""
-
-
-########to do other pieces
